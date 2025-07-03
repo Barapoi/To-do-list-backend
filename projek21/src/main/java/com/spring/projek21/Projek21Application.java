a198388e75b0a2299ff3d3d191b69b4fff3b90e3
Barapoi <faridalbaraka608@gmail.com>
2025-07-03 13:15:24 +0700

    Add files via upload

---
+++ projek21/src/main/java/com/spring/projek21/Projek21Application.java
@@ -0,0 +1,13 @@
+package com.spring.projek21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projek21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projek21Application.class, args);
+	}
+
+}
