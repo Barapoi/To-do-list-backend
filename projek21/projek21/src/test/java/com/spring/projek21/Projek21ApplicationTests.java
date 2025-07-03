30777459506fa45877113f1e9b9bfeb560d63f4f
Barapoi <faridalbaraka608@gmail.com>
2025-07-03 21:51:49 +0700

    Add files via upload
    fix run

---
+++ projek21/projek21/src/test/java/com/spring/projek21/Projek21ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.projek21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = Projek21Application.class)
+class Projek21ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
